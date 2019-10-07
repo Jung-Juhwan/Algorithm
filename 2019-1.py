952aeb4bfc28463834de0185269f68a3f1e929de
Jung-Juhwan <44596066+Jung-Juhwan@users.noreply.github.com>
2019-10-08 00:59:03 +0900

    Add files via upload

---
+++ 2019-1.py
@@ -0,0 +1,18 @@
+def solution(record):
+    answer = []
+    userDict = dict()
+    chatLog = []
+    enterMsg = "%s님이 들어왔습니다."
+    exitMsg = "%s님이 나갔습니다."
+    for info in record:
+        infoLst = info.split(' ')
+        if infoLst[0] == 'Enter':
+            userDict[infoLst[1]] = infoLst[2]
+            chatLog.append([enterMsg, infoLst[1]])
+        elif infoLst[0] == 'Leave':
+            chatLog.append([exitMsg, infoLst[1]])
+        elif infoLst[0] == 'Change':
+            userDict[infoLst[1]] = infoLst[2]
+    for log in chatLog:
+        answer.append(log[0] % userDict[log[1]])
+    return answer
